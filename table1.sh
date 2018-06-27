2596b49f163b1a5a6a6dcdd4f0cee8cf7570dbbf
Matthew Krafczyk <krafczyk.matthew@gmail.com>
2018-06-27 06:58:29 -0500

    Add script for producing table 1.

---
+++ table1.sh
@@ -0,0 +1,4 @@
+./example1 -l 9 -a 0.1666667 -N 8 2>/dev/null > table1.csv
+./example1 -l 9 -a 0.1666667 -N 16 2>/dev/null >> table1.csv
+./example1 -l 9 -a 0.1666667 -N 32 2>/dev/null >> table1.csv
+./example1 -l 9 -a 0.1666667 -N 64 2>/dev/null >> table1.csv
