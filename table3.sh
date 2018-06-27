fbca7e8bb6e327e3c92d1e6c48b2dac11fc89564
Matthew Krafczyk <krafczyk.matthew@gmail.com>
2018-06-27 07:26:39 -0500

    Add scripts for tables 3 and 4.

---
+++ table3.sh
@@ -0,0 +1,17 @@
+./example2 -l 9 --alpha 0.16666667 -N 32 2>/dev/null > table3.csv
+#./example2 -l 9 --alpha 0.16666667 -N 64 2>/dev/null >> table3.csv
+#./example2 -l 9 --alpha 0.16666667 -N 128 2>/dev/null >> table3.csv
+#./example2 -l 9 --alpha 0.16666667 -N 256 2>/dev/null >> table3.csv
+#./example2 -l 9 --alpha 0.16666667 -N 512 2>/dev/null >> table3.csv
+
+./example2 -l 9 --alpha 0.5 -N 50 2>/dev/null >> table3.csv
+#./example2 -l 9 --alpha 0.5 -N 100 2>/dev/null >> table3.csv
+#./example2 -l 9 --alpha 0.5 -N 200 2>/dev/null >> table3.csv
+#./example2 -l 9 --alpha 0.5 -N 400 2>/dev/null >> table3.csv
+#./example2 -l 9 --alpha 0.5 -N 800 2>/dev/null >> table3.csv
+
+./example2 -l 9 --alpha 0.99 -N 50 2>/dev/null >> table3.csv
+#./example2 -l 9 --alpha 0.99 -N 100 2>/dev/null >> table3.csv
+#./example2 -l 9 --alpha 0.99 -N 200 2>/dev/null >> table3.csv
+#./example2 -l 9 --alpha 0.99 -N 400 2>/dev/null >> table3.csv
+#./example2 -l 9 --alpha 0.99 -N 800 2>/dev/null >> table3.csv
