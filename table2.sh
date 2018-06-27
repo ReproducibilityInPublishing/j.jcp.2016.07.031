74d4ac9983881d982c3ea6a75bb21d9586ac7223
Matthew Krafczyk <krafczyk.matthew@gmail.com>
2018-06-27 07:03:57 -0500

    Add table2 but don't perform enormous calculations

---
+++ table2.sh
@@ -0,0 +1,6 @@
+./example1 -l 2 -N 8192 -a 0.01 2>/dev/null > table2.csv
+./example1 -l 3 -N 8192 -a 0.01 2>/dev/null >> table2.csv
+./example1 -l 4 -N 8192 -a 0.01 2>/dev/null >> table2.csv
+./example1 -l 5 -N 8192 -a 0.01 2>/dev/null >> table2.csv
+#./example1 -l 6 -N 8192 -a 0.01 2>/dev/null >> table2.csv
+#./example1 -l 7 -N 8192 -a 0.01 2>/dev/null >> table2.csv
