85916dd69f16056d163ae135fc544ff355e4b8cf
Matthew Krafczyk <krafczyk.matthew@gmail.com>
2018-07-05 18:33:55 -0500

    Update table scripts to calculate all elements of the table.

--- table1.sh
+++ table1.sh
@@ -1,4 +1,6 @@
-./example1 -l 9 -a 0.1666667 -N 8 2>/dev/null > table1.csv
+"AIMGM" > table1.csv
+
+./example1 -l 9 -a 0.1666667 -N 8 2>/dev/null >> table1.csv
 ./example1 -l 9 -a 0.1666667 -N 16 2>/dev/null >> table1.csv
 ./example1 -l 9 -a 0.1666667 -N 32 2>/dev/null >> table1.csv
 ./example1 -l 9 -a 0.1666667 -N 64 2>/dev/null >> table1.csv
@@ -13,3 +15,19 @@
 ./example1 -l 9 -a 0.99 -N 400 2>/dev/null >> table1.csv
 ./example1 -l 9 -a 0.99 -N 800 2>/dev/null >> table1.csv
 
+"BD-ADI" >> table1.csv
+
+./example1_BDADI -l 9 -a 0.1666667 -N 8 2>/dev/null >> table1.csv
+./example1_BDADI -l 9 -a 0.1666667 -N 16 2>/dev/null >> table1.csv
+./example1_BDADI -l 9 -a 0.1666667 -N 32 2>/dev/null >> table1.csv
+./example1_BDADI -l 9 -a 0.1666667 -N 64 2>/dev/null >> table1.csv
+
+./example1_BDADI -l 9 -a 0.5 -N 32 2>/dev/null >> table1.csv
+./example1_BDADI -l 9 -a 0.5 -N 64 2>/dev/null >> table1.csv
+./example1_BDADI -l 9 -a 0.5 -N 128 2>/dev/null >> table1.csv
+./example1_BDADI -l 9 -a 0.5 -N 256 2>/dev/null >> table1.csv
+
+./example1_BDADI -l 9 -a 0.99 -N 100 2>/dev/null >> table1.csv
+./example1_BDADI -l 9 -a 0.99 -N 200 2>/dev/null >> table1.csv
+./example1_BDADI -l 9 -a 0.99 -N 400 2>/dev/null >> table1.csv
+./example1_BDADI -l 9 -a 0.99 -N 800 2>/dev/null >> table1.csv
--- table4.sh
+++ table4.sh
@@ -1,6 +1,17 @@
-./example2 -l 2 -N 15000 --alpha 0.01 2>/dev/null > table4.csv
+echo "AIMGM" > table4.csv
+
+./example2 -l 2 -N 15000 --alpha 0.01 2>/dev/null >> table4.csv
 ./example2 -l 3 -N 15000 --alpha 0.01 2>/dev/null >> table4.csv
 ./example2 -l 4 -N 15000 --alpha 0.01 2>/dev/null >> table4.csv
 ./example2 -l 5 -N 15000 --alpha 0.01 2>/dev/null >> table4.csv
 ./example2 -l 6 -N 15000 --alpha 0.01 2>/dev/null >> table4.csv
 ./example2 -l 7 -N 15000 --alpha 0.01 2>/dev/null >> table4.csv
+
+echo "BFSMGM" >> table4.csv
+
+./example2_BFSMGM -l 2 -N 15000 --alpha 0.01 2>/dev/null >> table4.csv
+./example2_BFSMGM -l 3 -N 15000 --alpha 0.01 2>/dev/null >> table4.csv
+./example2_BFSMGM -l 4 -N 15000 --alpha 0.01 2>/dev/null >> table4.csv
+./example2_BFSMGM -l 5 -N 15000 --alpha 0.01 2>/dev/null >> table4.csv
+./example2_BFSMGM -l 6 -N 15000 --alpha 0.01 2>/dev/null >> table4.csv
+./example2_BFSMGM -l 7 -N 15000 --alpha 0.01 2>/dev/null >> table4.csv
