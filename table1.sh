3973933660dd8d1c6ba4626466aa76b5aa008f30
August Shi <awshi2@illinois.edu>
2018-07-06 14:33:32 -0500

    Fixing script to properly echo

--- table1.sh
+++ table1.sh
@@ -1,4 +1,4 @@
-"AIMGM" > table1.csv
+echo "AIMGM" > table1.csv
 
 ./example1 -l 9 -a 0.1666667 -N 8 2>/dev/null >> table1.csv
 ./example1 -l 9 -a 0.1666667 -N 16 2>/dev/null >> table1.csv
@@ -15,7 +15,7 @@
 ./example1 -l 9 -a 0.99 -N 400 2>/dev/null >> table1.csv
 ./example1 -l 9 -a 0.99 -N 800 2>/dev/null >> table1.csv
 
-"BD-ADI" >> table1.csv
+echo "BD-ADI" >> table1.csv
 
 ./example1_BDADI -l 9 -a 0.1666667 -N 8 2>/dev/null >> table1.csv
 ./example1_BDADI -l 9 -a 0.1666667 -N 16 2>/dev/null >> table1.csv
