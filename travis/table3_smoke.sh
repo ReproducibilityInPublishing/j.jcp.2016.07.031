ed171839660a3b2beeaf62535b3ee0ff87c0cabb
August Shi <awshi2@illinois.edu>
2018-11-15 15:40:50 -0600

    Shortening smoke tests

--- travis/table3_smoke.sh
+++ travis/table3_smoke.sh
@@ -1,21 +1,15 @@
 echo "AIMGM" > table3_smoke.csv
 
 ../example2 -l 9 --alpha 0.16666667 -N 32 2>/dev/null >> table3_smoke.csv
-../example2 -l 9 --alpha 0.16666667 -N 64 2>/dev/null >> table3_smoke.csv
 
 ../example2 -l 9 --alpha 0.5 -N 50 2>/dev/null >> table3_smoke.csv
-../example2 -l 9 --alpha 0.5 -N 100 2>/dev/null >> table3_smoke.csv
 
 ../example2 -l 9 --alpha 0.99 -N 50 2>/dev/null >> table3_smoke.csv
-../example2 -l 9 --alpha 0.99 -N 100 2>/dev/null >> table3_smoke.csv
 
 echo "BFSMGM" >> table3_smoke.csv
 
 ../example2_BFSMGM -l 9 --alpha 0.16666667 -N 32 2>/dev/null >> table3_smoke.csv
-../example2_BFSMGM -l 9 --alpha 0.16666667 -N 64 2>/dev/null >> table3_smoke.csv
 
 ../example2_BFSMGM -l 9 --alpha 0.5 -N 50 2>/dev/null >> table3_smoke.csv
-../example2_BFSMGM -l 9 --alpha 0.5 -N 100 2>/dev/null >> table3_smoke.csv
 
 ../example2_BFSMGM -l 9 --alpha 0.99 -N 50 2>/dev/null >> table3_smoke.csv
-../example2_BFSMGM -l 9 --alpha 0.99 -N 100 2>/dev/null >> table3_smoke.csv
