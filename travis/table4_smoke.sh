35555bfc0c6eb485d455dbdac4e31800f546efae
August Shi <awshi2@illinois.edu>
2018-11-15 15:32:38 -0600

    Fixing example command path

--- travis/table4_smoke.sh
+++ travis/table4_smoke.sh
@@ -1,11 +1,11 @@
 echo "AIMGM" > table4_smoke.csv
 
-./example2 -l 2 -N 15000 --alpha 0.01 2>/dev/null >> table4_smoke.csv
-./example2 -l 3 -N 15000 --alpha 0.01 2>/dev/null >> table4_smoke.csv
-./example2 -l 4 -N 15000 --alpha 0.01 2>/dev/null >> table4_smoke.csv
+../example2 -l 2 -N 15000 --alpha 0.01 2>/dev/null >> table4_smoke.csv
+../example2 -l 3 -N 15000 --alpha 0.01 2>/dev/null >> table4_smoke.csv
+../example2 -l 4 -N 15000 --alpha 0.01 2>/dev/null >> table4_smoke.csv
 
 echo "BFSMGM" >> table4_smoke.csv
 
-./example2_BFSMGM -l 2 -N 15000 --alpha 0.01 2>/dev/null >> table4_smoke.csv
-./example2_BFSMGM -l 3 -N 15000 --alpha 0.01 2>/dev/null >> table4_smoke.csv
-./example2_BFSMGM -l 4 -N 15000 --alpha 0.01 2>/dev/null >> table4_smoke.csv
+../example2_BFSMGM -l 2 -N 15000 --alpha 0.01 2>/dev/null >> table4_smoke.csv
+../example2_BFSMGM -l 3 -N 15000 --alpha 0.01 2>/dev/null >> table4_smoke.csv
+../example2_BFSMGM -l 4 -N 15000 --alpha 0.01 2>/dev/null >> table4_smoke.csv
