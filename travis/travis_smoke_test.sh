49a586c30d34a41f50b947e31a3fabdff1cb0c3b
August Shi <awshi2@illinois.edu>
2018-11-26 12:59:27 -0600

    Attempting to allow failures for when comparing against paper values

--- travis/travis_smoke_test.sh
+++ travis/travis_smoke_test.sh
@@ -2,11 +2,16 @@
 set -e
 
 tabnum=$1
+paper=$2
 
 # Navigate into this directory
 cd "$( dirname "${BASH_SOURCE[0]}" )"
 
 echo "RUNNING TABLE ${tabnum}" && bash table${tabnum}_smoke.sh
 
-# Check their results
-bash check.sh expected_table${tabnum}_smoke.csv table${tabnum}_smoke.csv
+# Check their results, compare against paper or not
+if [[ ${paper} == 1 ]]; then
+    bash check.sh expected_table${tabnum}_smoke_paper.csv table${tabnum}_smoke.csv
+else
+    bash check.sh expected_table${tabnum}_smoke.csv table${tabnum}_smoke.csv
+fi
