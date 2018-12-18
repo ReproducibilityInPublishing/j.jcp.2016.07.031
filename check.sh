0bfc3500fe4df1f3e32fd6dc3070ed9bbdd4c837
August Shi <awshi2@illinois.edu>
2018-12-18 09:16:56 -0600

    Fixing location of expected

--- check.sh
+++ check.sh
@@ -5,7 +5,7 @@ for i in 1 2 3 4; do
     # Paste the table file and the expected table file together as to do a line by line diff
     IFS=$'\n'
     rownum=1    # Keep track of rows
-    for l in $(paste <(grep -v AIMGM table${i}.csv | grep -v BD-ADI) <(grep -v AIMGM expected_table${i}.csv | grep -v BD-ADI)); do
+    for l in $(paste <(grep -v AIMGM table${i}.csv | grep -v BD-ADI) <(grep -v AIMGM expected_tables/table${i}.csv | grep -v BD-ADI)); do
         # Do separate diff for each row (treat each row as a test)
         actual=$(echo ${l} | cut -d$'\t' -f1)
         expected=$(echo ${l} | cut -d$'\t' -f2)
